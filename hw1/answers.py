e2edcca11b32328ff71bc1dead7d896746b9f16d
Itay Segev <itaysegev11@gmail.com>
2023-04-03 09:12:46 +0300

    Part2 dry

--- hw1/answers.py
+++ hw1/answers.py
@@ -40,27 +40,33 @@ An equation: $e^{i\pi} -1 = 0$
 part2_q1 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Increasing the value of k in the KNN algorithm can lead to improved generalization for unseen data up to a certain point,
+beyond which the performance may start to degrade.
+When k is very small, the model may overfit to the training data and fail to capture the underlying patterns in the data,
+resulting in poor generalization to new data.
+On the other hand, when k is very large, the model may oversimplify the decision boundary and treat distant points as equally important as nearby points,
+which can also lead to poor generalization.
+In general, the optimal value of k depends on the complexity of the underlying data distribution,
+the noise level in the data, and the size of the training set.
+In practice, a common approach is to perform cross-validation to tune the hyperparameter k and choose the value that gives the best performance on a validation set.
 
 """
 
 part2_q2 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+The main advantage of k-fold CV is that it provides a more reliable estimate of the generalization performance of
+the model by simulating the process of training and testing on multiple independent subsets of the data
+By using k-fold CV, we can obtain a more accurate and unbiased estimate of the true performance of the model,
+as it accounts for the variability and randomness in the data and reduces the risk of overfitting to the train-set.
+In contrast, training on the entire train-set with various models and selecting the best model based on train-set accuracy can lead to overfitting to the train-set,
+as the model may learn the idiosyncrasies and noise in the train-set that do not generalize well to new data.
+Moreover, k-fold CV allows us to evaluate and compare the performance of different models or hyperparameters in a fair and systematic way, as all models are trained and tested on the same data subsets.
+This can help us select the best model or hyperparameters that optimize the performance on the unseen data.
+The main disadvantage of selecting the best model based on test-set accuracy is that it introduces bias in the model selection process,
+as the test set is no longer independent from the training process. By using the test set to select the best model,
+we are indirectly optimizing the model to perform well on the test set, which may not generalize well to new data.
+K-fold CV allows us to evaluate the performance of the model on multiple independent subsets of the data and obtain a more reliable estimate of the true performance. By using k-fold CV, we are simulating the process of training and testing on multiple independent train-test splits and averaging the performance across them. This can reduce the variability and randomness in the data and provide a more accurate estimate of the generalization performance of the model.
+Furthermore, k-fold CV allows us to use all the available data for training and evaluation, without sacrificing the effective sample size for model selection. This can lead to a more robust and generalizable model, as it is trained on more data and has learned the underlying patterns that are more likely to generalize to new data.
 """
 
 # ==============
