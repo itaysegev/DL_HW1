4e61ced77c85818fe1056fada2650aade0c0df15
Tom Smolin <tomsmo016@gmail.com>
2023-04-07 16:37:41 +0300

    implementation of the __init()__, predict() and evaluate_accuracy() functions in the LinearClassifier

--- hw1/linear_classifier.py
+++ hw1/linear_classifier.py
@@ -23,7 +23,7 @@ class LinearClassifier(object):
 
         self.weights = None
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        self.weights = torch.randn(n_classes, n_features) * weight_std
         # ========================
 
     def predict(self, x: Tensor):
@@ -45,7 +45,8 @@ class LinearClassifier(object):
 
         y_pred, class_scores = None, None
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        class_scores = torch.matmul(x, self.weights.transpose(0, 1))
+        _, y_pred = torch.max(class_scores, dim=1)
         # ========================
 
         return y_pred, class_scores
@@ -66,7 +67,7 @@ class LinearClassifier(object):
 
         acc = None
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        acc = torch.sum(y_pred == y).item() / y.shape[0]
         # ========================
 
         return acc * 100
