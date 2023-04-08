77830c9db1b051a61a9a9350243b4a3c262a5d4e
Itay Segev <itaysegev11@gmail.com>
2023-04-08 09:56:00 +0300

    Part1 and Part3 dry

--- hw1/answers.py
+++ hw1/answers.py
@@ -120,40 +120,43 @@ An equation: $e^{i\pi} -1 = 0$
 
 part4_q1 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+In an ideal residual plot, the residuals should be randomly scattered around the horizontal line at zero.
+This indicates that the model is capturing the underlying patterns in the data and the errors are random and not systematically biased.
+Based on the residual plots shown above, we can see that the model is not perfectly fitting the data.
+The residual plots show some patterns in the errors, particularly for the higher target values.
+This suggests that the model may be overfitting to some extent.
+Comparing the residual plot for the top-5 features with the final plot after CV,
+we can see that the latter has fewer patterns in the errors and the residuals are more randomly scattered around the zero line.
+This suggests that the final model after CV is a better fit to the data than the model based on just the top-5 features.
 """
 
 part4_q2 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Adding non-linear features to our data can capture more complex relationships between the independent and dependent variables.
+In linear regression, adding non-linear features can allow us to fit curves to the data, rather than just straight lines.
+This can improve the model's ability to capture non-linear trends and patterns in the data, leading to better predictions.
+However, adding non-linear features does not change the fact that we are still using a linear regression model.
+The model is still linear in the sense that the parameters are still multiplied by the features and summed to produce a prediction.
+The non-linear features simply allow the model to capture non-linear relationships between the features and the target variable.
+In theory, we can fit any non-linear function of the original features by adding non-linear features.
+However, in practice, we may need to experiment with different types and combinations of non-linear features to find the ones that work best for our specific problem.
+In a linear classification model,adding non-linear features would affect the decision boundary by allowing it to become
+more complex and potentially non-linear. The decision boundary would no longer be a hyperplane,as it would be in a linear model with linear features only.
+Instead, it would be a more complex boundary that could potentially curve and bend to better separate the different classes. This can improve the model's ability to classify complex and non-linear patterns in the data. However, as with linear regression,
+we may need to experiment with different types and combinations of non-linear features to find the ones that work best for our specific classification problem.
 
 """
 
 part4_q3 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+In the above CV code, np.logspace was used instead of np.linspace when defining the range for lambda
+because np.logspace creates a range of values that are spaced logarithmically.
+This is advantageous for CV because changing the value of lambda by a factor of 10 has a greater impact on the model's performance than changing it by a constant value. By using a logarithmic scale for lambda,
+we can explore a wide range of values with relatively few steps and obtain a better sense of the optimal value.
+The model was fitted to the data k_folds * len(lambda_range) * len(degree_range) times.
+In our case, we have 20 values for lambda and 3 values for degree, giving a total of 60 combinations of hyperparameters.
+For each combination, we perform 3-fold cross-validation, so the model is fitted to data 3 times per combination.
+Therefore, the model is fitted to data a total of 60 * 3 = 180 times, not including the final fit on the entire training set.
 
 """
 
